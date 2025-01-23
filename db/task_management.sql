188cfd27d9b47b30b0de719b603fd718fae67e58
afsana <afsana1996sultana@gmail.com>
2025-01-23 19:06:13 +0600

    README file update

--- db/task_management.sql
+++ db/task_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 23, 2025 at 05:37 AM
+-- Generation Time: Jan 23, 2025 at 01:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.1.25
 
@@ -96,7 +96,7 @@ CREATE TABLE `personal_access_tokens` (
 --
 
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
-(4, 'App\\Models\\User', 1, 'myApp', '6746e3e0e5d80156c387c9bd6b065d62dbe162eaa481b08eb05fb49eb36257de', '[\"*\"]', '2025-01-22 13:37:04', '2025-01-22 23:34:18', '2025-01-22 13:34:18', '2025-01-22 13:37:04');
+(6, 'App\\Models\\User', 1, 'myApp', '2b3ce0ffce09bb7b42a0f1c860c80d021bc98f363fdb1cc6eb2eca4ad484e33e', '[\"*\"]', '2025-01-23 06:22:30', '2025-01-23 15:32:05', '2025-01-23 05:32:05', '2025-01-23 06:22:30');
 
 -- --------------------------------------------------------
 
@@ -108,8 +108,9 @@ CREATE TABLE `tasks` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
-  `priority` enum('Low','Medium','High') NOT NULL DEFAULT 'Medium',
-  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0=>pending, 1=>completed',
+  `priority` enum('Low','Medium','High') DEFAULT 'Medium',
+  `status` tinyint(1) DEFAULT 0 COMMENT '0=>pending, 1=>completed',
+  `created_by` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -118,12 +119,24 @@ CREATE TABLE `tasks` (
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `title`, `description`, `priority`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Test Task', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', 'Low', 0, '2025-01-22 11:51:54', '2025-01-22 11:53:23'),
-(2, 'Test Task-2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'High', 1, '2025-01-22 11:52:17', '2025-01-22 11:52:17'),
-(3, 'Demo Task', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'High', 0, '2025-01-22 11:52:39', '2025-01-22 11:52:39'),
-(7, 'Api Task Check', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', 'Medium', 0, '2025-01-22 13:40:21', '2025-01-22 13:40:21'),
-(8, 'Assign Task', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', 'High', 0, '2025-01-22 13:40:47', '2025-01-22 13:40:47');
+INSERT INTO `tasks` (`id`, `title`, `description`, `priority`, `status`, `created_by`, `created_at`, `updated_at`) VALUES
+(1, 'Test Task', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', 'Low', 1, '2', '2025-01-22 11:51:54', '2025-01-23 03:47:28'),
+(2, 'Test Task-2', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'High', 1, '2', '2025-01-22 11:52:17', '2025-01-22 11:52:17'),
+(3, 'Demo Task', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'High', 0, '1', '2025-01-22 11:52:39', '2025-01-22 11:52:39'),
+(7, 'Api Task Check', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', 'Medium', 0, '1', '2025-01-22 13:40:21', '2025-01-22 13:40:21'),
+(8, 'Assign Task', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s.', 'High', 0, '4', '2025-01-22 13:40:47', '2025-01-22 13:40:47'),
+(9, 'Auth Task Check', 'Auth Task Check', 'Medium', 0, '1', '2025-01-23 03:31:22', '2025-01-23 03:31:22'),
+(10, 'Auth Task Check Lita', 'Auth Task Check Lita', 'High', 0, '3', '2025-01-23 03:32:42', '2025-01-23 03:32:42'),
+(11, 'Complete Task', 'Complete Task', 'Low', 1, '3', '2025-01-23 03:33:34', '2025-01-23 03:34:53'),
+(12, 'fgdgfdf', 'dfgdfgs', 'High', 0, '3', '2025-01-23 03:36:24', '2025-01-23 03:36:24'),
+(13, 'Task Request', 'Task Request', 'Low', 1, '3', '2025-01-23 03:41:55', '2025-01-23 03:41:55'),
+(14, 'tgg', 'dfgsd', 'Medium', 0, '3', '2025-01-23 03:42:06', '2025-01-23 03:42:06'),
+(16, 'UpdateTask', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s Update', 'Low', 1, '1', '2025-01-23 05:26:03', '2025-01-23 05:43:26'),
+(17, 'Api Check Task AuthCheck', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'Medium', 1, '1', '2025-01-23 05:26:47', '2025-01-23 05:26:47'),
+(18, 'Api Check Task AuthCheck', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'Medium', 1, '1', '2025-01-23 05:30:13', '2025-01-23 05:30:13'),
+(19, 'Api Check Task AuthCheck', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'Medium', 1, '1', '2025-01-23 05:32:45', '2025-01-23 05:32:45'),
+(20, 'Task AuthCheck', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'Medium', 0, '1', '2025-01-23 05:34:01', '2025-01-23 05:34:01'),
+(21, 'Task AuthCheck Requird', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s,', 'Medium', 0, '1', '2025-01-23 05:44:09', '2025-01-23 05:44:09');
 
 -- --------------------------------------------------------
 
@@ -149,7 +162,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, 'Afsana Sultana', 'afsana1996sultana@gmail.com', NULL, '$2y$10$msemFWikUI0Z7sUrTSPTvOs1w5c1A2Mpj3HBlMMoPcWuBucyDkxQ6', NULL, '2025-01-22 11:49:07', '2025-01-22 11:49:07'),
 (2, 'Azmira Sultana', 'azmira@gmail.com', NULL, '$2y$10$KDrdymeHY6K4z37kiV1c5e3x2tiQBdhblYTzmj5is5sgaWhO44vlS', NULL, '2025-01-22 13:51:09', '2025-01-22 13:51:09'),
-(3, 'Tanjila Sultana', 'tanjila@gmail.com', NULL, '$2y$10$1ienM19YDZlgV5FqBWg.KONd/BCjqCS/6sZ4Capz085QUWErRHzTi', NULL, '2025-01-22 13:52:09', '2025-01-22 13:52:09');
+(3, 'Tanjila Sultana', 'tanjila@gmail.com', NULL, '$2y$10$1ienM19YDZlgV5FqBWg.KONd/BCjqCS/6sZ4Capz085QUWErRHzTi', NULL, '2025-01-22 13:52:09', '2025-01-22 13:52:09'),
+(4, 'Medha', 'medha@gmail.com', NULL, '$2y$10$mHFeJ.Hv4xHIfEOJZJxyQeA.irw2PX56dflauzsftpyl4CaA8PIr6', NULL, '2025-01-23 00:48:16', '2025-01-23 00:48:16');
 
 --
 -- Indexes for dumped tables
@@ -215,19 +229,19 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
